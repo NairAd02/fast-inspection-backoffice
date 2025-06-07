e0909beac58115dece09a7792c7e32bec57381dd
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-07 18:19:25 -0400

    feat: display EmptyContent when no configurations are available

--- src/sections/configs/list/configs-list.tsx
+++ src/sections/configs/list/configs-list.tsx
@@ -1,6 +1,7 @@
 import { Config } from "@/lib/types/configs";
 import React from "react";
 import ConfigCard from "../components/config-card/config-card";
+import EmptyContent from "@/components/empty-content/empty-content";
 
 const configs: Config[] = [
   {
@@ -34,11 +35,16 @@ const configs: Config[] = [
 ];
 
 export default function ConfigsList() {
-  return (
+  return configs.length > 0 ? (
     <div className=" w-full grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
       {configs.map((config, index) => (
         <ConfigCard key={index} config={config} />
       ))}
     </div>
+  ) : (
+    <EmptyContent
+      title="No hay configuraciones registradas"
+      description="Todavía no se han registrado configuraciones en el sistema."
+    />
   );
 }
