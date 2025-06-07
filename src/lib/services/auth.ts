99202a67e1488a8bf0719c88c41651a5e7cec1e7
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-07 19:03:53 -0400

    feat(auth): implement login and signIn functions for user authentication

---
+++ src/lib/services/auth.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { signIn as signInAuth } from "@/auth";
+import { buildApiResponse } from "@/lib/api";
+import { apiRoutes } from "@/routes/api-routes/api-routes";
+import { User } from "next-auth";
+import { CredentialsDTO } from "../types/auth";
+
+export async function login(credentials: CredentialsDTO) {
+  const res = await fetch(apiRoutes.auth.login, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  });
+
+  return buildApiResponse<User>(res);
+}
+
+export async function signIn(credentials: CredentialsDTO) {
+  return signInAuth("credentials", {
+    username: credentials.username,
+    password: credentials.password,
+    redirect: false,
+  });
+}
