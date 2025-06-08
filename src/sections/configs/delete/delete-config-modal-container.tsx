5d1f5c754c4e1c646c4e07b9f8d86683cb2f03d8
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-08 15:57:49 -0400

    feat(configs): add DeleteConfigModalContainer for configuration deletion confirmation

---
+++ src/sections/configs/delete/delete-config-modal-container.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { ModalContext } from "@/components/modal/context/modalContext";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import React, { useCallback, useContext } from "react";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+import { revalidateServerTags } from "@/lib/cache";
+import { tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
+import { toast } from "react-toastify";
+import useDeleteConfig from "../hooks/use-delete-config";
+
+export default function DeleteConfigModalContainer() {
+  const { getInfoModal, handleCloseModal } = useContext(ModalContext);
+  const infoModal = getInfoModal(modalTypes.deleteConfigModal.name);
+  const id = infoModal && infoModal.entity ? infoModal.entity : null;
+  const { deleteConfig, loading } = useDeleteConfig({
+    id,
+    onDeleteAction: () => {
+      toast.success("Configuración eliminada con éxito");
+      onCloseModal();
+      revalidateServerTags(tagsCacheByRoutes.configs.multipleTag);
+    },
+  });
+  const onCloseModal = useCallback(() => {
+    handleCloseModal(modalTypes.deleteConfigModal.name);
+  }, [handleCloseModal]);
+  return (
+    <ConfirmationPanel
+      title={
+        modalTypes.deleteConfigModal.title || "Elminación de la Configuración"
+      }
+      message={modalTypes.deleteConfigModal.message}
+      warningMessage={modalTypes.deleteConfigModal.warningMessage}
+      onConfirm={deleteConfig}
+      onCancel={onCloseModal}
+      isLoading={loading}
+      isDestructive
+    />
+  );
+}
