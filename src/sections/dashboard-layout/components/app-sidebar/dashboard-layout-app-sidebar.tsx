324592fe0ba41c1ddd32e11ef03d98adefa44d42
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-07 22:31:23 -0400

    refactor(sidebar): remove NavUserContainer and associated user navigation components

--- src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarHeader,
   SidebarRail,
 } from "@/components/ui/sidebar";
 import { groupRoutes } from "@/routes/groups-routes/groups-routes";
 import SidebarGroupNavigation from "@/sections/dashboard-layout/components/app-sidebar/sidebar-group-navigation/sidebar-group-navigation";
-import { NavUserContainer } from "../nav-user/nav-user-container";
 
 export function DashBoardLayoutAppSidebar({
   ...props
@@ -21,9 +19,6 @@ export function DashBoardLayoutAppSidebar({
           <SidebarGroupNavigation key={index} group={groupRoute} />
         ))}
       </SidebarContent>
-      <SidebarFooter>
-        <NavUserContainer />
-      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
--- src/sections/dashboard-layout/components/nav-user/nav-user-container.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import NavUser from "./nav-user";
-
-export function NavUserContainer() {
-  return <NavUser user={{ username: "admin", email: "admin@admin.admin" }} />;
-}
