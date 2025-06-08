8f81fb7c7b0ebe96ce7e5de988fb9c84ab0816f5
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-08 12:24:14 -0400

    feat(configs): add createConfigByOtherConfig route to apiRoutes

--- src/routes/api-routes/api-routes.ts
+++ src/routes/api-routes/api-routes.ts
@@ -6,6 +6,7 @@ export const apiRoutes = {
     get: `${process.env.NEXT_PUBLIC_API_URL}configs/getAllConfigs`,
     getById: `${process.env.NEXT_PUBLIC_API_URL}config/:id`,
     createNewConfig: `${process.env.NEXT_PUBLIC_API_URL}configs/createNewConfig`,
+    createConfigByOtherConfig: `${process.env.NEXT_PUBLIC_API_URL}configs/createConfigByOtherConfig/{versionOtherConfig}`
   },
 } as const;
 
