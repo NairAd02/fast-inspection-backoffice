0c90ee616609b5a4f8fcc88709491dd8b081b61d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-07 21:29:27 -0400

    feat(sign-in): implement sign-in functionality with form validation and hooks

---
+++ src/sections/sign-in/form/hooks/use-sign-in.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useCallback, useState } from "react";
+import { Credentials } from "../schemas/credentials-schema";
+import { signIn as nextAuthSignIn } from "next-auth/react";
+
+interface Props {
+  onSignInAction: () => void;
+}
+
+export default function useSignIn({ onSignInAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const signIn = useCallback(
+    async (credentials: Credentials) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await nextAuthSignIn("credentials", {
+          username: credentials.firstCredential,
+          password: credentials.password,
+          redirect: false,
+        });
+        if (res.error)
+          setError("Las credenciales proporcionadas no son correctas");
+        else onSignInAction();
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onSignInAction]
+  );
+  return {
+    loading,
+    error,
+    signIn,
+  };
+}
