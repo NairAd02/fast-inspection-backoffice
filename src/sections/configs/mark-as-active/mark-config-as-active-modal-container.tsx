2d0d90272f4da7ea1a4b999ed88ef45274bde2aa
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-08 16:26:10 -0400

    feat(modal): implement MarkConfigAsActive modal container for configuration activation

---
+++ src/sections/configs/mark-as-active/mark-config-as-active-modal-container.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { ModalContext } from "@/components/modal/context/modalContext";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import React, { useCallback, useContext } from "react";
+import ConfirmationPanel from "@/components/confirmation-panel/confirmation-panel";
+import { revalidateServerTags } from "@/lib/cache";
+import { tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
+import { toast } from "react-toastify";
+import useMarkConfigAsActive from "../hooks/use-mark-config-as-active";
+
+export default function MarkConfigAsActiveModalContainer() {
+  const { getInfoModal, handleCloseModal } = useContext(ModalContext);
+  const infoModal = getInfoModal(modalTypes.markConfigAsActiveModal.name);
+  const id = infoModal && infoModal.entity ? infoModal.entity : null;
+  const { markConfigAsActive, loading } = useMarkConfigAsActive({
+    id,
+    onMarkConfigAsActiveAction: () => {
+      toast.success("Configuración activada con éxito");
+      onCloseModal();
+      revalidateServerTags(tagsCacheByRoutes.configs.multipleTag);
+    },
+  });
+  const onCloseModal = useCallback(() => {
+    handleCloseModal(modalTypes.markConfigAsActiveModal.name);
+  }, [handleCloseModal]);
+  return (
+    <ConfirmationPanel
+      title={
+        modalTypes.markConfigAsActiveModal.title ||
+        "Activación de la Configuración"
+      }
+      message={modalTypes.markConfigAsActiveModal.message}
+      warningMessage={modalTypes.markConfigAsActiveModal.warningMessage}
+      onConfirm={markConfigAsActive}
+      onCancel={onCloseModal}
+      isLoading={loading}
+    />
+  );
+}
