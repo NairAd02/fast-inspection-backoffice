f84a080cb7a69a41f3216a710f3e1d96e96bad2f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-10 13:15:30 -0400

    feat(ConfigsPage): refactor modal components into Configlayout for better organization

---
+++ src/app/(protected)/dashboard/configs/layout.tsx
@@ -0,0 +1,52 @@
+import Modal from "@/components/modal/modal";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import DeleteConfigModalContainer from "@/sections/configs/delete/delete-config-modal-container";
+import EditConfigModalContainer from "@/sections/configs/form/edit/edit-config-modal-container";
+import NewConfigFormContainer from "@/sections/configs/form/new/new-config-form-container";
+import ReplicateConfigModalContainer from "@/sections/configs/form/replicate/replicate-config-modal-container";
+import MarkConfigAsActiveModalContainer from "@/sections/configs/mark-as-active/mark-config-as-active-modal-container";
+import React from "react";
+
+export default function Configlayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      {children}
+      <Modal
+        formPath={modalTypes.newConfigModal.name}
+        title={modalTypes.newConfigModal.title}
+      >
+        <NewConfigFormContainer />
+      </Modal>
+      <Modal
+        formPath={modalTypes.editConfigModal.name}
+        title={modalTypes.editConfigModal.title}
+        className="min-h-[36vh]"
+      >
+        <EditConfigModalContainer />
+      </Modal>
+      <Modal
+        formPath={modalTypes.deleteConfigModal.name}
+        title={modalTypes.deleteConfigModal.title}
+      >
+        <DeleteConfigModalContainer />
+      </Modal>
+      <Modal
+        formPath={modalTypes.markConfigAsActiveModal.name}
+        title={modalTypes.markConfigAsActiveModal.title}
+      >
+        <MarkConfigAsActiveModalContainer />
+      </Modal>
+      <Modal
+        formPath={modalTypes.replicateConfigModal.name}
+        title={modalTypes.replicateConfigModal.title}
+        className="min-h-[36vh]"
+      >
+        <ReplicateConfigModalContainer />
+      </Modal>
+    </>
+  );
+}
--- src/app/(protected)/dashboard/configs/page.tsx
+++ src/app/(protected)/dashboard/configs/page.tsx
@@ -1,12 +1,5 @@
-import Modal from "@/components/modal/modal";
-import { modalTypes } from "@/components/modal/types/modalTypes";
 import { SearchParamsPagination } from "@/lib/types/pagination";
 import ConfigsContainer from "@/sections/configs/configs-container";
-import DeleteConfigModalContainer from "@/sections/configs/delete/delete-config-modal-container";
-import EditConfigModalContainer from "@/sections/configs/form/edit/edit-config-modal-container";
-import NewConfigFormContainer from "@/sections/configs/form/new/new-config-form-container";
-import ReplicateConfigModalContainer from "@/sections/configs/form/replicate/replicate-config-modal-container";
-import MarkConfigAsActiveModalContainer from "@/sections/configs/mark-as-active/mark-config-as-active-modal-container";
 import React from "react";
 
 type Props = {
@@ -17,38 +10,6 @@ export default async function ConfigsPage({ searchParams }: Props) {
   return (
     <>
       <ConfigsContainer searchParams={await searchParams} />
-      <Modal
-        formPath={modalTypes.newConfigModal.name}
-        title={modalTypes.newConfigModal.title}
-      >
-        <NewConfigFormContainer />
-      </Modal>
-      <Modal
-        formPath={modalTypes.editConfigModal.name}
-        title={modalTypes.editConfigModal.title}
-        className="min-h-[36vh]"
-      >
-        <EditConfigModalContainer />
-      </Modal>
-      <Modal
-        formPath={modalTypes.deleteConfigModal.name}
-        title={modalTypes.deleteConfigModal.title}
-      >
-        <DeleteConfigModalContainer />
-      </Modal>
-      <Modal
-        formPath={modalTypes.markConfigAsActiveModal.name}
-        title={modalTypes.markConfigAsActiveModal.title}
-      >
-        <MarkConfigAsActiveModalContainer />
-      </Modal>
-      <Modal
-        formPath={modalTypes.replicateConfigModal.name}
-        title={modalTypes.replicateConfigModal.title}
-        className="min-h-[36vh]"
-      >
-        <ReplicateConfigModalContainer />
-      </Modal>
     </>
   );
 }
