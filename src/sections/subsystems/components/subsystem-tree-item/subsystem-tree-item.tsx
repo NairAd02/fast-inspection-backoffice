4b5cf8196cffc6db6f0d5486fd74d849c5ff1b0d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-10 21:47:44 -0400

    feat(component): add SubsystemTreeItem component for displaying subsystem details

---
+++ src/sections/subsystems/components/subsystem-tree-item/subsystem-tree-item.tsx
@@ -0,0 +1,25 @@
+import { Badge } from "@/components/ui/badge";
+import { Subsystem } from "@/lib/types/subsystems";
+import { CogIcon } from "lucide-react";
+import React from "react";
+
+interface Props {
+  subsystem: Subsystem;
+}
+
+export default function SubsystemTreeItem({ subsystem }: Props) {
+  return (
+    <div className="flex items-center gap-3">
+      <CogIcon className="h-4 w-4 text-green-600" />
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center gap-2">
+          <span className="font-medium text-green-900">{subsystem.nombre}</span>
+          <Badge variant={"default"}>{"SUB-" + subsystem.id}</Badge>
+          <Badge variant={"secondary"}>
+            {"Cantidad de subsistemas: " + subsystem.cantMateriales}
+          </Badge>
+        </div>
+      </div>
+    </div>
+  );
+}
