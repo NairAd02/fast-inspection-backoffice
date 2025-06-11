a0d66b5d8bfd899136487af34ea34a1cd9b21582
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-10 21:40:41 -0400

    feat(component): add SystemTreeItem component for displaying system details

---
+++ src/sections/systems/components/system-tree-item/system-tree-item.tsx
@@ -0,0 +1,30 @@
+import { Badge } from "@/components/ui/badge";
+import { System } from "@/lib/types/systems";
+import { BuildingIcon } from "lucide-react";
+import React from "react";
+
+interface Props {
+  system: System;
+}
+
+export default function SystemTreeItem({ system }: Props) {
+  return (
+    <div className="flex items-center gap-3">
+      <BuildingIcon className="h-5 w-5 text-blue-600" />
+      <div className="flex flex-col gap-1">
+        <div className="flex items-center gap-2">
+          <Badge variant={"default"} className="font-semibold">
+            {system.nombre}
+          </Badge>
+          <Badge variant={"default"}>{"S-" + system.id}</Badge>
+          <Badge variant={"secondary"}>
+            {"Cantidad de subsistemas: " + system.cantSubsistemas}
+          </Badge>
+        </div>
+        <span className="text-sm text-muted-foreground">
+          Herramienta: {system.herramienta.nombre} ({system.herramienta.tipo})
+        </span>
+      </div>
+    </div>
+  );
+}
