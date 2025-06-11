4ad17117d41dc7e17c53b14162058e4c95448f7e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-10 23:05:42 -0400

    fix(page): remove unused getConfigById import and related error handling

--- src/app/(protected)/dashboard/configs/[id]/page.tsx
+++ src/app/(protected)/dashboard/configs/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { getConfigById } from "@/lib/services/configs";
 import ConfigManagementContainer from "@/sections/configs/config-management/config-management-container";
 import React from "react";
 
@@ -8,9 +7,6 @@ type Props = {
 
 export default async function ConfigManagementPage({ params }: Props) {
   const id = (await params).id;
-  const { response, error } = await getConfigById(id);
-  if (!response || error) throw new Error("Error fetching config");
-  const config = response;
   return (
     <>
       <ConfigManagementContainer version={id} />
