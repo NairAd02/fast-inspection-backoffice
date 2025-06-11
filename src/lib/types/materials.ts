db3d875017991b6a3cf4848032d407b6f3931300
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-10 21:51:29 -0400

    feat(types): add Material interface for material representation

--- src/lib/types/materials.ts
+++ src/lib/types/materials.ts
@@ -1,5 +1,11 @@
 import { DeteriorationType } from "./deterioration-type";
 
+export interface Material {
+  id: number;
+  nombre: string;
+  cantTiposDeterioros: number;
+}
+
 export interface MaterialDetails {
   id: number;
   nombre: string;
