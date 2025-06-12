1cdfeb571d5d63cec25109c9f408caf784a8c6f0
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-11 23:22:59 -0400

    feat(hook): add useToolsFilters hook for managing tool filters

---
+++ src/sections/tools/filters/hooks/use-tools-filters.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import useUrlFilters from "@/hooks/use-url-filters";
+import { Pagination } from "@/lib/types/pagination";
+import { convertToolsFiltersDTO } from "@/lib/types/tools";
+import { Dispatch, SetStateAction, useState } from "react";
+
+export interface ToolsFilters {
+  nombre?: string;
+}
+
+interface Props {
+  setPagination?: Dispatch<SetStateAction<Pagination>>;
+}
+
+export default function useToolsFilters({ setPagination }: Props) {
+  const { updateFiltersInUrl } = useUrlFilters();
+  const [filters, setFilters] = useState<ToolsFilters>({});
+
+  async function handleChangeFilters(updatedFilters: Partial<ToolsFilters>) {
+    const newFilters = {
+      ...filters,
+      ...updatedFilters,
+    };
+    await setFilters((prev) => ({
+      ...prev,
+      ...updatedFilters,
+    }));
+    updateFiltersInUrl(convertToolsFiltersDTO(newFilters));
+    if (setPagination)
+      setPagination((oldPagination) => ({ ...oldPagination, page: 1 }));
+  }
+
+  function handleResetFilters() {
+    setFilters({});
+    updateFiltersInUrl({});
+    if (setPagination)
+      setPagination((oldPagination) => ({ ...oldPagination, page: 1 }));
+  }
+
+  const getActiveFiltersCount = () => {
+    let count = 0;
+    if (filters.nombre) count++;
+
+    return count;
+  };
+
+  return {
+    filters,
+    handleChangeFilters,
+    handleResetFilters,
+    getActiveFiltersCount,
+  };
+}
