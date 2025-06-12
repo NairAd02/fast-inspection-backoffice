63cb40073e727d839efc0406b73063cc040897b4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 10:29:33 -0400

    feat(api-routes): add delete route for systems configuration

--- src/routes/api-routes/api-routes.ts
+++ src/routes/api-routes/api-routes.ts
@@ -16,6 +16,7 @@ export const apiRoutes = {
     getById: `${process.env.NEXT_PUBLIC_API_URL}sistemas-config/getSistemaConfig/:id`,
     create: `${process.env.NEXT_PUBLIC_API_URL}sistemas-config/createSistemaConfig`,
     edit: `${process.env.NEXT_PUBLIC_API_URL}sistemas-config/updateSistemaConfig/:id`,
+    delete: `${process.env.NEXT_PUBLIC_API_URL}sistemas-config/deleteSistemaConfig/:id`
   },
   tools: {
     get: `${process.env.NEXT_PUBLIC_API_URL}herramienta-analisis-criticidad/getAllHerramientasAnalisisCriticidad`,
