e0635f8621d614a98609e78b8c4900ecdaf33461
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 11:01:32 -0400

    feat(subsystems): implement useCreateSubsystem hook for subsystem creation

---
+++ src/sections/subsystems/hooks/use-create-subsystem.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useCallback, useState } from "react";
+import { SubsystemCreate } from "../form/new/schemas/subsystem-create-schema";
+import { createSubsystem as createSubsystemService } from "@/lib/services/subsystems";
+import { convertSubsystemCreateDTO } from "@/lib/types/subsystems";
+
+interface Props {
+  onCreateAction: () => void;
+}
+
+export default function useCreateSubsystem({ onCreateAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createSubsystem = useCallback(
+    async (subsystem: SubsystemCreate, systemId: string) => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await createSubsystemService(
+          convertSubsystemCreateDTO(subsystem, systemId)
+        );
+        if (!res.response || res.error) {
+          console.log(res.error);
+          setError("Error en la creación del subsistema");
+        } else {
+          onCreateAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onCreateAction]
+  );
+  return {
+    loading,
+    error,
+    createSubsystem,
+  };
+}
