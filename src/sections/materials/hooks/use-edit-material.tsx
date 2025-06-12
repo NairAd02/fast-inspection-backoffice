161912a178c7d3e81855f79b69056cbd9c608045
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 16:46:34 -0400

    feat(materials): implement useEditMaterial hook for editing materials

---
+++ src/sections/materials/hooks/use-edit-material.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useCallback, useState } from "react";
+import { editMaterial as editMaterialService } from "@/lib/services/materials";
+import { convertMaterialEditDTO } from "@/lib/types/materials";
+import { MaterialEdit } from "../form/edit/schemas/material-edit-schema";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditMaterial({ id, onEditAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editMaterial = useCallback(
+    async (material: MaterialEdit) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await editMaterialService(
+          id,
+          convertMaterialEditDTO(material)
+        );
+        if (!res.response || res.error)
+          setError("Error en la edición del material");
+        else {
+          onEditAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editMaterial,
+  };
+}
