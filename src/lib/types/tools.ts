07334f76a719ac4274850f82a71dc24e11715e77
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-11 23:22:30 -0400

    feat(types): add ToolsFiltersDTO interface and convertToolsFiltersDTO function

--- src/lib/types/tools.ts
+++ src/lib/types/tools.ts
@@ -1,5 +1,19 @@
+import { ToolsFilters } from "@/sections/tools/filters/hooks/use-tools-filters";
+
 export interface Tool {
   id: number;
   nombre: string;
   tipo: string;
 }
+
+export interface ToolsFiltersDTO {
+  nombre?: string;
+}
+
+export const convertToolsFiltersDTO = (
+  filters: ToolsFilters
+): ToolsFiltersDTO => {
+  return {
+    ...filters,
+  };
+};
