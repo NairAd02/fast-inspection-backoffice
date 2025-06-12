2ee8a2eedc837f86ef925899c3586d840277958d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 17:01:45 -0400

    feat(materials): add getMaterialById function for retrieving material by ID

--- src/lib/services/materials.ts
+++ src/lib/services/materials.ts
@@ -6,8 +6,21 @@ import {
   MaterialCreateDTO,
   MaterialEditDTO,
 } from "../types/materials";
-import { apiRoutes } from "@/routes/api-routes/api-routes";
+import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
 import { buildApiResponse } from "../api";
+
+export async function getMaterialById(id: string) {
+  const session = await auth();
+  const res = await fetch(apiRoutes.materials.getById.replace(":id", id), {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+    },
+    next: { tags: [tagsCacheByRoutes.materials.singleTag + ": " + id] },
+  });
+
+  return await buildApiResponse<Material>(res);
+}
 
 export async function createMaterial(materialCreateDTO: MaterialCreateDTO) {
   const session = await auth();
