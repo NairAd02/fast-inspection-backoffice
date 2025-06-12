2d31b0b13c3e1910c9abaac93313747e42f1c184
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 00:08:51 -0400

    feat(api): add editSystem function for updating existing systems

--- src/lib/services/systems.ts
+++ src/lib/services/systems.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { auth } from "@/auth";
-import { System, SystemCreateDTO } from "../types/systems";
+import { System, SystemCreateDTO, SystemEditDTO } from "../types/systems";
 import { apiRoutes } from "@/routes/api-routes/api-routes";
 import { buildApiResponse } from "../api";
 
@@ -14,6 +14,20 @@ export async function createSystem(systemCreateDTO: SystemCreateDTO) {
       "content-type": "application/json",
     },
     body: JSON.stringify(systemCreateDTO),
+  });
+
+  return await buildApiResponse<System>(res);
+}
+
+export async function editSystem(id: string, systemEditDTO: SystemEditDTO) {
+  const session = await auth();
+  const res = await fetch(apiRoutes.systems.edit.replace(":id", id), {
+    method: "PATCH",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(systemEditDTO),
   });
 
   return await buildApiResponse<System>(res);
