ffff44ad51cacce573a3ffd8bd6dce26f6bd516a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-12 00:11:54 -0400

    feat(hook): implement useEditSystem hook for editing system details

---
+++ src/sections/systems/hooks/use-edit-system.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { useCallback, useState } from "react";
+import { SystemEdit } from "../form/edit/schemas/system-edit-schema";
+import { editSystem as editSystemService } from "@/lib/services/systems";
+import { convertSystemEditDTO } from "@/lib/types/systems";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditSystem({ id, onEditAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editSystem = useCallback(
+    async (system: SystemEdit) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await editSystemService(id, convertSystemEditDTO(system));
+        if (!res.response || res.error)
+          setError("Error en la edición del sistema");
+        else {
+          onEditAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editSystem,
+  };
+}
