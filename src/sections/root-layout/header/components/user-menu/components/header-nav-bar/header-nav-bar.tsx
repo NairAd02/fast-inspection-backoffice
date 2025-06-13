d01b1a34c355331e3286044da5c3fa4ab2fc13a2
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-13 19:22:43 -0400

    feat(header-nav-bar): refactor navigation structure and integrate OptionsLinksContainer

--- src/sections/root-layout/header/components/user-menu/components/header-nav-bar/header-nav-bar.tsx
+++ src/sections/root-layout/header/components/user-menu/components/header-nav-bar/header-nav-bar.tsx
@@ -5,40 +5,29 @@ import React from "react";
 import UserMenu from "../../user-menu";
 import NavigationComponent from "@/components/navigation-component/navigation-component";
 import { paths } from "@/routes/path";
+import OptionsLinksContainer from "./options-links/options-links-container";
 
 export default function HeaderNavBar() {
   const { data: session, status } = useSession();
   return status === "loading" ? (
     <></>
   ) : (
-    <nav className="hidden md:flex items-center space-x-6">
-      <a
-        href="#features"
-        className="text-gray-600 hover:text-gray-900 transition-colors"
-      >
-        Características
-      </a>
-      <a
-        href="#process"
-        className="text-gray-600 hover:text-gray-900 transition-colors"
-      >
-        Proceso
-      </a>
-      <a
-        href="#pricing"
-        className="text-gray-600 hover:text-gray-900 transition-colors"
-      >
-        Precios
-      </a>
-      {!session && (
+    <nav className="flex flex-row md:flex-row-reverse items-center gap-2 md:gap-6">
+      <div className="flex items-center gap-2 md:gap-6">
+        {!session && (
         <NavigationComponent href={paths.sign_in.root}>
           <Button variant="outline" size="sm">
             Iniciar Sesión
           </Button>
         </NavigationComponent>
       )}
+     
       <Button size="sm">Comenzar</Button>
-      {session && <UserMenu />}
+       {session && <UserMenu />}
+      </div>
+      <div>
+        <OptionsLinksContainer />
+      </div>
     </nav>
   );
 }
