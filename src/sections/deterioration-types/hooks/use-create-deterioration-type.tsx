04bdbedc0fd7185181128b99769b5ebdae5c1f59
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-13 23:00:10 -0400

    feat(deterioration-types): implement useCreateDeteriorationType hook for managing deterioration type creation

---
+++ src/sections/deterioration-types/hooks/use-create-deterioration-type.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useCallback, useState } from "react";
+import { DeteriorationTypeCreate } from "../form/new/schemas/deterioration-type-create-schema";
+import { createDeteriorationType as createDeteriorationTypeService } from "@/lib/services/deterioration-types";
+import { convertDeteriorationTypeCreateDTO } from "@/lib/types/deterioration-type";
+
+interface Props {
+  onCreateAction: () => void;
+}
+
+export default function useCreateDeteriorationType({ onCreateAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createDeteriorationType = useCallback(
+    async (deteriorationType: DeteriorationTypeCreate, materialId: string) => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await createDeteriorationTypeService(
+          convertDeteriorationTypeCreateDTO(deteriorationType, materialId)
+        );
+        if (!res.response || res.error) {
+          console.log(res.error);
+          setError("Error en la creación del tipo de deterioro");
+        } else {
+          onCreateAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onCreateAction]
+  );
+  return {
+    loading,
+    error,
+    createDeteriorationType,
+  };
+}
