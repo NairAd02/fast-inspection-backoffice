e0b08868518f50280ff51337c25a883ce41b9590
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-14 14:40:08 -0400

    feat(rhf-stack-causa): create RHFStackCausa component for cause name input

---
+++ src/sections/deterioration-types/form/rhf-components/rhf-stack-causa/rhf-stack-causa.tsx
@@ -0,0 +1,18 @@
+import { RHFTextField } from "@/components/form/rhf-components/rhf-text-field/rhf-text-field";
+import React from "react";
+
+interface Props {
+  index: number;
+}
+
+export default function RHFStackCausa({ index }: Props) {
+  return (
+    <div className="flex items-center">
+      <RHFTextField
+        name={`causa.${index}.nombre`}
+        placeholder="Introduzca el nombre de la causa"
+        fullWidth
+      />
+    </div>
+  );
+}
