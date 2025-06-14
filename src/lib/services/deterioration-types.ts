d1772556e673f8619a75db7bf55d4e879626f709
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-13 20:43:21 -0400

    feat(deterioration-types): implement createDeteriorationType function for API integration

---
+++ src/lib/services/deterioration-types.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { auth } from "@/auth";
+import {
+  DeteriorationType,
+  DeteriorationTypeCreateDTO,
+} from "../types/deterioration-type";
+import { apiRoutes } from "@/routes/api-routes/api-routes";
+import { buildApiResponse } from "../api";
+
+export async function createDeteriorationType(
+  deteriorationTypeCreateDTO: DeteriorationTypeCreateDTO
+) {
+  const session = await auth();
+  const res = await fetch(apiRoutes.deteriorationTypes.create, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(deteriorationTypeCreateDTO),
+  });
+
+  return await buildApiResponse<DeteriorationType>(res);
+}
