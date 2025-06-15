4518081c33f3ac9a8672cdfceac764872d9bc710
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-15 11:20:31 -0400

    refactor(deterioration-types): rename NewDeteriorationTypeModalContainer and update imports for clarity

--- src/sections/deterioration-types/form/new/new-deterioration-type-form-modal-container.tsx
+++ src/sections/deterioration-types/form/new/new-deterioration-type-form-modal-container.tsx
@@ -1,77 +1,20 @@
 "use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import React, { useContext } from "react";
-import { FormProvider, useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
 import { ModalContext } from "@/components/modal/context/modalContext";
 import { modalTypes } from "@/components/modal/types/modalTypes";
-import { toast } from "react-toastify";
-import { RevalidateConfigInformationContext } from "@/sections/configs/context/revalidate-config-information-context/revalidate-config-information-context";
-import {
-  DeteriorationTypeCreate,
-  deteriorationTypeCreateSchema,
-} from "./schemas/deterioration-type-create-schema";
-import useCreateDeteriorationType from "../../hooks/use-create-deterioration-type";
-import DeteriorationTypeForm from "../deterioration-type-form";
+import React, { useContext } from "react";
+import NewDeteriorationFormContainer from "./new-deterioration-type-form-container";
 
-interface Props {
-  materialId: string;
-  toolId: string;
-}
 
-export default function NewDeteriorationFormContainer({
-  materialId,
-  toolId,
-}: Props) {
-  const { handleCloseModal } = useContext(ModalContext);
-  const { revalidateConfigInformation } = useContext(
-    RevalidateConfigInformationContext
-  );
-  const { loading: submitLoading, createDeteriorationType } =
-    useCreateDeteriorationType({
-      onCreateAction: () => {
-        toast.success("Tipo de Deterioro creado con éxito");
-        handleClose();
-        revalidateConfigInformation();
-      },
-    });
-  const form = useForm<DeteriorationTypeCreate>({
-    resolver: zodResolver(deteriorationTypeCreateSchema),
-    defaultValues: {
-      nombre: "",
-      detectabilidad: 1,
-      causas: [],
-      camposAfectados: [],
-      camposDefinidosImagen: [],
-      camposDefinidosNumericos: [],
-      camposDefinidosSeleccion: [],
-      camposDefinidosTexto: [],
-    },
-  });
-
-  const handleClose = () => {
-    handleCloseModal(modalTypes.newDeteriorationTypeModal.name);
-  };
-
-  function onSubmit(deteriorationTypeCreate: DeteriorationTypeCreate) {
-    createDeteriorationType(deteriorationTypeCreate, materialId);
-  }
+export default function NewDeteriorationTypeModalContainer() {
+  const { getInfoModal } = useContext(ModalContext);
+  const infoModal = getInfoModal(modalTypes.newDeteriorationTypeModal.name);
+  const id = infoModal && infoModal.entity ? infoModal.entity : null;
+  const toolId =
+    infoModal && infoModal.secondaryEntity ? infoModal.secondaryEntity : null;
   return (
-    <FormProvider {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
-      >
-        <DeteriorationTypeForm toolId={toolId} />
-        <div className="flex gap-2 justify-end">
-          <Button type="button" variant={"destructive"} onClick={handleClose}>
-            Cancelar
-          </Button>
-          <Button variant={"default"} type="submit" disabled={submitLoading}>
-            Crear Tipo de Deterioro
-          </Button>
-        </div>
-      </form>
-    </FormProvider>
+    <NewDeteriorationFormContainer
+      materialId={id as string}
+      toolId={toolId as string}
+    />
   );
 }
