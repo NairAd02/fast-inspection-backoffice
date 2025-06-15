5a6e5ed82e64f39f597b42b950181c0d63478e09
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-15 15:30:17 -0400

    fix(deterioration-types): adjust container width for DeteriorationTypeDetailsContainer

--- src/sections/deterioration-types/details/deterioration-type-details-container.tsx
+++ src/sections/deterioration-types/details/deterioration-type-details-container.tsx
@@ -14,7 +14,7 @@ export default function DeteriorationTypeDetailsContainer({
   deteriorationType,
 }: Props) {
   return (
-    <div className="w-full max-w-4xl mx-auto p-6 bg-white rounded-lg">
+    <div className="w-full mx-auto p-6 bg-white rounded-lg">
       {/* Header Section */}
       <DeteriorationTypeDetailsHeader deteriorationType={deteriorationType} />
 
