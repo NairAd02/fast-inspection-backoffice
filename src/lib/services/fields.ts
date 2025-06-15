c296c14f140970a4b0b748a2cbd7e60364dbd131
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-14 23:19:59 -0400

    feat(fields): implement getFieldsList function for fetching fields with authentication

---
+++ src/lib/services/fields.ts
@@ -0,0 +1,24 @@
+"use server";
+import { auth } from "@/auth";
+import { IQueryable } from "../types/request";
+import { QueryParamsURLFactory } from "../request";
+import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
+import { buildApiResponse } from "../api";
+import { PaginationResponse } from "../types/pagination";
+import { Field } from "react-hook-form";
+
+export async function getFieldsList(params: IQueryable) {
+  const session = await auth();
+
+  const url = new QueryParamsURLFactory(params, apiRoutes.fields.get).build();
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+    },
+    next: { tags: [tagsCacheByRoutes.fields.multipleTag] },
+  });
+
+  return await buildApiResponse<PaginationResponse<Field>>(res);
+}
