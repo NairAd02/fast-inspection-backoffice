bd0dadf53dcd37a87e55f93839cc99735ea16471
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-15 12:05:27 -0400

    feat(deterioration-types): add custom hook for editing deterioration types

---
+++ src/sections/deterioration-types/hooks/use-edit-deterioration-type.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useCallback, useState } from "react";
+import { DeteriorationTypeEdit } from "../form/edit/schemas/deterioration-type-edit-schema";
+import { editDeteriorationType as editDeteriorationTypeService } from "@/lib/services/deterioration-types";
+import { convertDeteriorationTypeEditDTO } from "@/lib/types/deterioration-type";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditDeteriorationType({ id, onEditAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editDeteriorationType = useCallback(
+    async (deteriorationType: DeteriorationTypeEdit) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await editDeteriorationTypeService(
+          id,
+          convertDeteriorationTypeEditDTO(deteriorationType)
+        );
+        if (!res.response || res.error)
+          setError("Error en la edición del tipo de deterioro");
+        else {
+          onEditAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editDeteriorationType,
+  };
+}
