367461bf5ffb7527bfe30516da39b4da754b3729
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-17 17:11:06 -0400

    feat(calculable-indices): add a map of calculable index types with names and colors

--- src/lib/types/calculable-indices.ts
+++ src/lib/types/calculable-indices.ts
@@ -3,6 +3,30 @@ export enum TipoIndiceCalculable {
   IndiceCalcuableSinIntervalo = "indiceCalculableSinIntervalo",
 }
 
+export const tiposIndiceCalculableTypeMap: Map<
+  TipoIndiceCalculable,
+  {
+    name: string;
+    color:
+      | "default"
+      | "primary"
+      | "secondary"
+      | "error"
+      | "info"
+      | "success"
+      | "warning";
+  }
+> = new Map([
+  [
+    TipoIndiceCalculable.InidiceCalculableIntervalo,
+    { name: "Indice Calculable con Intervalo", color: "primary" },
+  ],
+  [
+    TipoIndiceCalculable.IndiceCalcuableSinIntervalo,
+    { name: "Indice Calculable sin Intervalo", color: "primary" },
+  ],
+]);
+
 export enum Calculos {
   Detectabilidad = "Detectabilidad",
   Impacto = "Impacto",
