397dcc8b3befb16affc29c08a3087fe1f4476a79
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-17 17:40:22 -0400

    feat(calculable-interval-indices): add a component to display and manage active filters on calculable indexes

---
+++ src/sections/calculable-indices/calculable-interval-indices/filters/calculable-interval-indices-active-filters/calculable-interval-indices-active-filters.tsx
@@ -0,0 +1,61 @@
+import { Label } from "@/components/ui/label";
+import React from "react";
+import FilterBadge from "@/components/filters/filter-badge/filter-badge";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { RotateCcwIcon } from "lucide-react";
+import { CalculableIntervalIndicesFilters } from "../hooks/use-calculable-interval-indices-filters";
+
+interface Props {
+  filters: CalculableIntervalIndicesFilters;
+  handleChangeFilters: (
+    filters: Partial<CalculableIntervalIndicesFilters>
+  ) => void;
+  getActiveFiltersCount: () => number;
+  handleResetFilters: () => void;
+}
+
+export default function CalculableIntervalIndicesActiveFilters({
+  filters,
+  handleChangeFilters,
+  getActiveFiltersCount,
+  handleResetFilters,
+}: Props) {
+  return (
+    <div>
+      <div className="space-y-4">
+        <div className="flex gap-2 items-center">
+          <Label>Filtros Activos</Label>
+          {getActiveFiltersCount() > 0 && (
+            <Badge variant="secondary" className="ml-2">
+              {getActiveFiltersCount()}
+            </Badge>
+          )}
+          {getActiveFiltersCount() > 0 && (
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={handleResetFilters}
+              className="h-8"
+            >
+              <RotateCcwIcon className="h-4 w-4 mr-1" />
+              Limpiar
+            </Button>
+          )}
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          {filters.nombre && (
+            <FilterBadge
+              filterName="Nombre"
+              filterValue={filters.nombre}
+              handleDeleteFilter={() => {
+                handleChangeFilters({ nombre: undefined });
+              }}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
