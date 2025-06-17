1dc56aea4fe4a93633b7dec6d542cc6718806def
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-17 12:04:32 -0400

    feat(calculable-indices): add enums for calculable index types and calculations

---
+++ src/lib/types/calculable-indices.ts
@@ -0,0 +1,11 @@
+export enum TipoIndiceCalculable {
+  InidiceCalculableIntervalo = "indiceCalculableIntervalo",
+  IndiceCalcuableSinIntervalo = "indiceCalculableSinIntervalo",
+}
+
+export enum Calculos {
+  Detectabilidad = "Detectabilidad",
+  Impacto = "Impacto",
+  Frecuencia = "Frecuencia",
+  Criticidad = "Criticidad",
+}
