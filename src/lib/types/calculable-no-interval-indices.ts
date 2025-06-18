5ae222d1cf4074d33546fa3708a7836fd8738889
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-17 22:18:21 -0400

    feat(calculable-no-interval-indices): add DTO and function to convert non-calculable index creation data without intervals

--- src/lib/types/calculable-no-interval-indices.ts
+++ src/lib/types/calculable-no-interval-indices.ts
@@ -1,5 +1,7 @@
 import { CalculableNoIntervalIndicesFilters } from "@/sections/calculable-indices/calculable-no-interval-indices/filters/hooks/use-calculable-no-interval-indices-filters";
 import { Calculos, TipoIndiceCalculable } from "./calculable-indices";
+import { IndicatorNoIntervalCreateDTO } from "./indicator-no-interval";
+import { CalculableNoIntervalIndiceCreate } from "@/sections/calculable-indices/calculable-no-interval-indices/form/new/schemas/calculable-no-interval-indice-create-schema";
 
 export interface CalculableNoIntervalIndice {
   id: number;
@@ -9,10 +11,31 @@ export interface CalculableNoIntervalIndice {
   configVersion: number;
 }
 
+export interface CalculableNoIntervalIndiceCreateDTO {
+  nombre: string;
+  indicadoresSinIntervalos: IndicatorNoIntervalCreateDTO[];
+  calculo: Calculos;
+  config: {
+    version: string;
+  };
+}
+
 export interface CalculableNoIntervalIndiceFiltersDTO {
   nombre?: string;
   versionConfig?: string;
 }
+
+export const convertCalculableNoIntervalIndiceCreateDTO = (
+  calculableNoIntervalIndiceCreate: CalculableNoIntervalIndiceCreate,
+  configVersion: string
+): CalculableNoIntervalIndiceCreateDTO => {
+  return {
+    ...calculableNoIntervalIndiceCreate,
+    config: {
+      version: configVersion,
+    },
+  };
+};
 
 export const convertCalculableNoIntervalIndiceFiltersDTO = (
   filters: CalculableNoIntervalIndicesFilters
