df3d90ed9016a8b24020145c3c0df03aafb8ce63
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-18 19:03:44 -0400

    feat(calculable-interval-indices): add DTO for creating calculable intervals and conversion function

--- src/lib/types/calculable-interval-indices.ts
+++ src/lib/types/calculable-interval-indices.ts
@@ -1,5 +1,7 @@
 import { CalculableIntervalIndicesFilters } from "@/sections/calculable-indices/calculable-interval-indices/filters/hooks/use-calculable-interval-indices-filters";
 import { Calculos, TipoIndiceCalculable } from "./calculable-indices";
+import { IndicatorIntervalCreateDTO } from "./indicator-interval";
+import { CalculableIntervalIndiceCreate } from "@/sections/calculable-indices/calculable-interval-indices/form/new/schemas/calculable-interval-indice-create-schema";
 
 export interface CalculableIntervalIndice {
   id: number;
@@ -9,10 +11,31 @@ export interface CalculableIntervalIndice {
   configVersion: number;
 }
 
+export interface CalculableIntervalIndiceCreateDTO {
+  nombre: string;
+  indicadoresIntervalos: IndicatorIntervalCreateDTO[];
+  calculo: Calculos;
+  config: {
+    version: string;
+  };
+}
+
 export interface CalculableIntervalIndiceFiltersDTO {
   nombre?: string;
   versionConfig?: string;
 }
+
+export const convertCalculableIntervalIndiceCreateDTO = (
+  calculableIntervalIndiceCreate: CalculableIntervalIndiceCreate,
+  configVersion: string
+): CalculableIntervalIndiceCreateDTO => {
+  return {
+    ...calculableIntervalIndiceCreate,
+    config: {
+      version: configVersion,
+    },
+  };
+};
 
 export const convertCalculableIntervalIndiceFiltersDTO = (
   filters: CalculableIntervalIndicesFilters
