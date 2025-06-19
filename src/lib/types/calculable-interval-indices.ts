72ffd2d71fadc8d38d7a4d7d2dbc86ddbad85864
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-18 23:51:25 -0400

    feat(calculable-interval-indices): add interface and function to edit calculable indexes

--- src/lib/types/calculable-interval-indices.ts
+++ src/lib/types/calculable-interval-indices.ts
@@ -1,7 +1,11 @@
 import { CalculableIntervalIndicesFilters } from "@/sections/calculable-indices/calculable-interval-indices/filters/hooks/use-calculable-interval-indices-filters";
 import { Calculos, TipoIndiceCalculable } from "./calculable-indices";
-import { IndicatorIntervalCreateDTO } from "./indicator-interval";
+import {
+  IndicatorIntervalCreateDTO,
+  IndicatorIntervalEditDTO,
+} from "./indicator-interval";
 import { CalculableIntervalIndiceCreate } from "@/sections/calculable-indices/calculable-interval-indices/form/new/schemas/calculable-interval-indice-create-schema";
+import { CalculableIntervalIndiceEdit } from "@/sections/calculable-indices/calculable-interval-indices/form/edit/schemas/calculable-interval-indice-edit-schema";
 
 export interface CalculableIntervalIndice {
   id: number;
@@ -20,6 +24,12 @@ export interface CalculableIntervalIndiceCreateDTO {
   };
 }
 
+export interface CalculableIntervalIndiceEditDTO {
+  nombre: string;
+  indicadoresIntervalos: IndicatorIntervalEditDTO[];
+  calculo: Calculos;
+}
+
 export interface CalculableIntervalIndiceFiltersDTO {
   nombre?: string;
   versionConfig?: string;
@@ -34,6 +44,14 @@ export const convertCalculableIntervalIndiceCreateDTO = (
     config: {
       version: configVersion,
     },
+  };
+};
+
+export const convertCalculableIntervalIndiceEditDTO = (
+  calculableIntervalIndiceEdit: CalculableIntervalIndiceEdit
+): CalculableIntervalIndiceEditDTO => {
+  return {
+    ...calculableIntervalIndiceEdit,
   };
 };
 
