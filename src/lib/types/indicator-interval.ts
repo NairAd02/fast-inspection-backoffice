788f78eae9a849f6297affe418190c6c62cf08a1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-18 23:55:47 -0400

    feat(indicator-interval): add new IndicatorInterval interface to define indicator intervals

--- src/lib/types/indicator-interval.ts
+++ src/lib/types/indicator-interval.ts
@@ -1,3 +1,11 @@
+export interface IndicatorInterval {
+  id: number;
+  nombre: string;
+  valor: number;
+  limiteInferior: number;
+  limiteSuperior?: number;
+}
+
 export interface IndicatorIntervalCreateDTO {
   nombre: string;
   valor: number;
