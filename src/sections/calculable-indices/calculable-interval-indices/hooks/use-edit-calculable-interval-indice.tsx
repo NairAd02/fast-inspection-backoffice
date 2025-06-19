34a77892e989cefdd3e0666bd9fd1905180215a9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-19 00:06:33 -0400

    feat(calculable-interval-indices): create a hook to edit calculable indexes

---
+++ src/sections/calculable-indices/calculable-interval-indices/hooks/use-edit-calculable-interval-indice.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useCallback, useState } from "react";
+import { CalculableIntervalIndiceEdit } from "../form/edit/schemas/calculable-interval-indice-edit-schema";
+import { editCalculableIntervalIndice as editCalculableIntervalIndiceService } from "@/lib/services/calculable-interval-indices";
+import { convertCalculableIntervalIndiceEditDTO } from "@/lib/types/calculable-interval-indices";
+
+interface Props {
+  id: string;
+  onEditAction: () => void;
+}
+
+export default function useEditCalculableIntervalIndice({
+  id,
+  onEditAction,
+}: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const editCalculableIntervalIndice = useCallback(
+    async (calculableIntervalIndice: CalculableIntervalIndiceEdit) => {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await editCalculableIntervalIndiceService(
+          id,
+          convertCalculableIntervalIndiceEditDTO(calculableIntervalIndice)
+        );
+        if (!res.response || res.error)
+          setError("Error en la edición de la índice calculable");
+        else {
+          onEditAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onEditAction, id]
+  );
+  return {
+    loading,
+    error,
+    editCalculableIntervalIndice,
+  };
+}
