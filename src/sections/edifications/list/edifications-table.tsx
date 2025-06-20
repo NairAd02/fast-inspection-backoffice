ed43f2fcb94d2935422a7340172f2ee80d97ee98
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 18:47:38 -0400

    feat(edifications): add a table component to display the list of buildings with actions

---
+++ src/sections/edifications/list/edifications-table.tsx
@@ -0,0 +1,110 @@
+"use client";
+import React from "react";
+import { useCallback, useContext } from "react";
+import { ModalContext } from "@/components/modal/context/modalContext";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import { Edification } from "@/lib/types/edifications";
+import { DataTable } from "@/components/ui/data-table";
+import TableMenu from "@/components/ui/table-menu";
+import { EditIcon, EyeIcon, MapPinIcon, Trash2Icon } from "lucide-react";
+import { ColumnDef } from "@tanstack/react-table";
+
+interface Props {
+  edifications: Edification[];
+  centeredMap: (id: number) => void;
+}
+
+export default function EdificationTable({ edifications, centeredMap }: Props) {
+  const { handleOpenModal } = useContext(ModalContext);
+
+  const handleEdit = useCallback(
+    (id: string) => {
+      handleOpenModal({ name: modalTypes.editConfigModal.name, entity: id });
+    },
+    [handleOpenModal]
+  );
+
+  const handleViewDetails = useCallback(
+    (id: string) => {
+      handleOpenModal({
+        name: modalTypes.detailsEdificationModal.name,
+        entity: id,
+      });
+    },
+    [handleOpenModal]
+  );
+
+  const handleDelete = useCallback(
+    (id: string) => {
+      handleOpenModal({ name: modalTypes.deleteConfigModal.name, entity: id });
+    },
+    [handleOpenModal]
+  );
+
+  const columns: ColumnDef<Edification>[] = [
+    {
+      accessorKey: "id",
+      enableHiding: false,
+    },
+    {
+      accessorKey: "nombre",
+      header: "Nombre",
+    },
+    {
+      accessorKey: "direccion",
+      header: "Dirección",
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        return (
+          <div className="flex justify-end">
+            <TableMenu
+              titleTableMenu="Acciones"
+              actions={[
+                {
+                  label: "Ver Detalles",
+                  icon: <EyeIcon />,
+                  action: () => {
+                    handleViewDetails(row.getValue("id"));
+                  },
+                },
+                {
+                  label: "Centrar en mapa",
+                  icon: <MapPinIcon />,
+                  action: () => {
+                    centeredMap(row.getValue("id"));
+                  },
+                },
+                {
+                  label: "Editar",
+                  icon: <EditIcon />,
+                  action: () => {
+                    handleEdit(row.getValue("id"));
+                  },
+                },
+                {
+                  label: "Eliminar",
+                  icon: <Trash2Icon />,
+                  action: () => {
+                    handleDelete(row.getValue("id"));
+                  },
+                },
+              ]}
+            />
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-4">
+      <DataTable
+        columns={columns}
+        data={edifications}
+        initialVisibilityState={{ id: false }}
+      />
+    </div>
+  );
+}
