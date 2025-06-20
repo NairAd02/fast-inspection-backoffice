afdb7f82ef259723b015c5c6ce87b5c44e8ec6a1
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 02:20:39 -0400

    feat(edifications): add EdificationDetails interface to represent building details with inspections

--- src/lib/types/edifications.ts
+++ src/lib/types/edifications.ts
@@ -1,7 +1,20 @@
+import { Inspection } from "./inspections";
+
 export interface Edification {
   id: number;
   nombre: string;
   direccion: string;
   coordX: number;
   coordY: number;
+}
+
+export interface EdificationDetails {
+  id: number;
+  nombre: string;
+  direccion: string;
+  coordX: number;
+  coordY: number;
+  inspecciones: {
+    data: Inspection[];
+  };
 }
