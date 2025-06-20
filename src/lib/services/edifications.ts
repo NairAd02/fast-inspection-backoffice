b981e28df8c710f4593e0a17447ffbfe0f849885
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 02:28:19 -0400

    feat(edifications): implement a function to get details of a building by ID

--- src/lib/services/edifications.ts
+++ src/lib/services/edifications.ts
@@ -6,7 +6,7 @@ import { QueryParamsURLFactory } from "../request";
 import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
 import { buildApiResponse } from "../api";
 import { PaginationResponse } from "../types/pagination";
-import { Edification } from "../types/edifications";
+import { Edification, EdificationDetails } from "../types/edifications";
 
 export async function getEdificationsList(params: IQueryable) {
   const session = await auth();
@@ -25,4 +25,17 @@ export async function getEdificationsList(params: IQueryable) {
   });
 
   return await buildApiResponse<PaginationResponse<Edification>>(res);
+}
+
+export async function getEdificationById(id: string) {
+  const session = await auth();
+  const res = await fetch(apiRoutes.edifications.getById.replace(":id", id), {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+    },
+    next: { tags: [tagsCacheByRoutes.edifications.singleTag + ": " + id] },
+  });
+
+  return await buildApiResponse<EdificationDetails>(res);
 }
