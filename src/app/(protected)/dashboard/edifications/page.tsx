f08ed94d1f2dd1071515bfb29f3f1f9a1bc11e02
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 18:49:07 -0400

    feat(edifications): add building details modal to the buildings page

--- src/app/(protected)/dashboard/edifications/page.tsx
+++ src/app/(protected)/dashboard/edifications/page.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import EdificationsContainer from "@/sections/edifications/edifications-container";
 import { SearchParamsPagination } from "@/lib/types/pagination";
+import Modal from "@/components/modal/modal";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import EdificationDetailsModalContainer from "@/sections/edifications/details/edification-details-modal-container";
 
 type Props = {
   searchParams: Promise<SearchParamsPagination>;
 };
 
 export default async function EdificationsPage({ searchParams }: Props) {
-  return <EdificationsContainer searchParams={await searchParams} />;
+  return (
+    <>
+      <EdificationsContainer searchParams={await searchParams} />;
+      <Modal
+        formPath={modalTypes.detailsEdificationModal.name}
+        title={modalTypes.detailsEdificationModal.title}
+        maxWidth="max-w-2xl"
+        className="min-h-[70vh] max-h-[90vh]"
+      >
+        <EdificationDetailsModalContainer />
+      </Modal>
+    </>
+  );
 }
