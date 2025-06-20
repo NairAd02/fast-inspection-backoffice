223f651815649bec8b35612409985146a127a8bc
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 00:03:44 -0400

    feat(edifications): implement function to get list of buildings with authentication

---
+++ src/lib/services/edifications.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { auth } from "@/auth";
+import { IQueryable } from "../types/request";
+import { QueryParamsURLFactory } from "../request";
+import { apiRoutes, tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
+import { buildApiResponse } from "../api";
+import { PaginationResponse } from "../types/pagination";
+import { Edification } from "../types/edifications";
+
+export async function getEdificationsList(params: IQueryable) {
+  const session = await auth();
+
+  const url = new QueryParamsURLFactory(
+    params,
+    apiRoutes.edifications.get
+  ).build();
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + session?.accessToken,
+    },
+    next: { tags: [tagsCacheByRoutes.edifications.multipleTag] },
+  });
+
+  return await buildApiResponse<PaginationResponse<Edification>>(res);
+}
