a4f58f777779ccb665991a768cacebba7ac47ec6
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-21 00:31:40 -0400

    feat(edifications): add hook for building creation

---
+++ src/sections/edifications/hooks/use-create-edification.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { useCallback, useState } from "react";
+import { createEdification as createEdificationService } from "@/lib/services/edifications";
+import { EdificationCreate } from "../form/new/schemas/edification-create-schema";
+import { convertEdificationCreateDTO } from "@/lib/types/edifications";
+
+interface Props {
+  onCreateAction: () => void;
+}
+
+export default function useCreateEdification({ onCreateAction }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const createEdification = useCallback(
+    async (edification: EdificationCreate) => {
+      try {
+        setLoading(true);
+        setError(null);
+
+        const res = await createEdificationService(
+          convertEdificationCreateDTO(edification)
+        );
+        if (!res.response || res.error) {
+          console.log(res.error);
+          setError("Error en la creación de la edificación");
+        } else {
+          onCreateAction();
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [onCreateAction]
+  );
+  return {
+    loading,
+    error,
+    createEdification,
+  };
+}
