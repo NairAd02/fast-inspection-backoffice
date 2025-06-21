c7e37efee668b4191eeac04b663dc57a4213185b
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-20 23:09:54 -0400

    feat(edifications): create a hook to manage building filters with pagination

---
+++ src/sections/edifications/filters/hooks/use-edifications-filters.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import useUrlFilters from "@/hooks/use-url-filters";
+import { convertEdificationsFiltersDTO } from "@/lib/types/edifications";
+import { Pagination } from "@/lib/types/pagination";
+import { convertToolsFiltersDTO } from "@/lib/types/tools";
+import { Dispatch, SetStateAction, useState } from "react";
+
+export interface EdificationsFilters {
+  nombre?: string;
+  direccion?: string;
+}
+
+interface Props {
+  setPagination?: Dispatch<SetStateAction<Pagination>>;
+  defaultsFilters?: EdificationsFilters;
+  urlPagination?: boolean;
+}
+
+export default function useEdificationsFilters({
+  setPagination,
+  defaultsFilters = {},
+  urlPagination = false,
+}: Props) {
+  const { updateFiltersInUrl } = useUrlFilters();
+  const [filters, setFilters] = useState<EdificationsFilters>(defaultsFilters);
+
+  async function handleChangeFilters(updatedFilters: Partial<EdificationsFilters>) {
+    const newFilters = {
+      ...filters,
+      ...updatedFilters,
+    };
+    await setFilters((prev) => ({
+      ...prev,
+      ...updatedFilters,
+    }));
+    if (urlPagination) updateFiltersInUrl(convertEdificationsFiltersDTO(newFilters));
+    if (setPagination)
+      setPagination((oldPagination) => ({ ...oldPagination, page: 1 }));
+  }
+
+  function handleResetFilters() {
+    setFilters({});
+    if (urlPagination) updateFiltersInUrl({});
+    if (setPagination)
+      setPagination((oldPagination) => ({ ...oldPagination, page: 1 }));
+  }
+
+  const getActiveFiltersCount = () => {
+    let count = 0;
+    if (filters.nombre) count++;
+    if (filters.direccion) count++;
+
+    return count;
+  };
+
+  return {
+    filters,
+    handleChangeFilters,
+    handleResetFilters,
+    getActiveFiltersCount,
+  };
+}
