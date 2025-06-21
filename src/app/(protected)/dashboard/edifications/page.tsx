4e76638c8cf2e4e7453b7ad05f1c75e9d6edd0d9
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-21 11:54:55 -0400

    feat(edifications): add a modal to create a new building on the buildings page

--- src/app/(protected)/dashboard/edifications/page.tsx
+++ src/app/(protected)/dashboard/edifications/page.tsx
@@ -4,6 +4,7 @@ import { SearchParamsPagination } from "@/lib/types/pagination";
 import Modal from "@/components/modal/modal";
 import { modalTypes } from "@/components/modal/types/modalTypes";
 import EdificationDetailsModalContainer from "@/sections/edifications/details/edification-details-modal-container";
+import NewEdificationFormContainer from "@/sections/edifications/form/new/new-edification-form-container";
 
 type Props = {
   searchParams: Promise<SearchParamsPagination>;
@@ -13,6 +14,13 @@ export default async function EdificationsPage({ searchParams }: Props) {
   return (
     <>
       <EdificationsContainer searchParams={await searchParams} />
+      <Modal
+        formPath={modalTypes.newEdificationModal.name}
+        title={modalTypes.newEdificationModal.title}
+        maxWidth="max-w-2xl"
+      >
+        <NewEdificationFormContainer />
+      </Modal>
       <Modal
         formPath={modalTypes.detailsEdificationModal.name}
         title={modalTypes.detailsEdificationModal.title}
