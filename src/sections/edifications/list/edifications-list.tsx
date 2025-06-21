7339038484e803439d89c1f461d0e624ca013975
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-21 12:28:58 -0400

    refactor(edifications): relocate the "Building Map" header in the building list

--- src/sections/edifications/list/edifications-list.tsx
+++ src/sections/edifications/list/edifications-list.tsx
@@ -14,9 +14,6 @@ export default function EdificationsList({ edifications }: Props) {
   const customMapWrapperRef = useRef<CustomMapWrapperRef>(null);
   return (
     <div className="p-6 mb-6 bg-white flex flex-col gap-2 rounded-lg shadow-sm">
-      <h1 className="text-2xl font-bold text-gray-800 text-center">
-        Mapa de Edificaciones
-      </h1>
       <EdificationTable
         edifications={edifications}
         centeredMap={(id: number) => {
@@ -25,6 +22,9 @@ export default function EdificationsList({ edifications }: Props) {
           }
         }}
       />
+      <h1 className="text-2xl font-bold text-gray-800 text-center">
+        Mapa de Edificaciones
+      </h1>
       <CustomMapWrapper
         ref={customMapWrapperRef}
         points={edifications.map((e) => ({
