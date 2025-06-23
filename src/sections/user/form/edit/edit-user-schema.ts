cf6e1a7e0e25c330e7b34a81779cc0879eb87c1f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-23 12:51:57 -0400

    refactor(schema): redefines the user editing schema to include field and role type validations

--- src/sections/user/form/edit/edit-user-schema.ts
+++ src/sections/user/form/edit/edit-user-schema.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
-import { NewUserSchema } from "../new/new-user-schema";
+import { Role } from "@/lib/types/user";
 
-export type EditUserSchema = z.infer<typeof EditUserSchema>
-export const EditUserSchema = NewUserSchema.partial()
+export type EditUserSchema = z.infer<typeof EditUserSchema>;
+export const EditUserSchema = z.object({
+  nombreUsuario: z.string().min(1, "El nombre de usuario es obligatorio"),
+  contrasena: z
+    .string()
+    .min(8, "La contraseña debe tener almenos 8 caracteres")
+    .optional(),
+  email: z.string().email("El email debe ser válido"),
+  rol: z.enum(Role),
+});
