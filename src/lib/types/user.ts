9cbe5bbea15628f3f70b89590f8fce98ef885371
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-23 13:01:05 -0400

    refactor(user): adds the UserEditDTO interface and the convertUserEditDTO function to improve user editing management

--- src/lib/types/user.ts
+++ src/lib/types/user.ts
@@ -1,21 +1,39 @@
+import { EditUserSchema } from "@/sections/user/form/edit/edit-user-schema";
 
-export type Role = (typeof Role)[number]
+export type Role = (typeof Role)[number];
 export const Role = [
-    "Súper Administrador", "Administrador", "Especialista Ing Civil", "Especialista Ing Civil Avanzado"
-] as const
+  "Súper Administrador",
+  "Administrador",
+  "Especialista Ing Civil",
+  "Especialista Ing Civil Avanzado",
+] as const;
 
 export type User = {
-    "id": string,
-    "nombreUsuario": string,
-    "email": string,
-    "rol": string,
-    "isActiva": boolean
-}
+  id: string;
+  nombreUsuario: string;
+  email: string;
+  rol: string;
+  isActiva: boolean;
+};
 
 export type UserDetails = {
-    "id": string,
-    "nombreUsuario": string,
-    "email": string,
-    "rol": string,
-    "isActiva": boolean
+  id: string;
+  nombreUsuario: string;
+  email: string;
+  rol: string;
+  isActiva: boolean;
+};
+
+export interface UserEditDTO {
+  nombreUsuario: string;
+  email: string;
+  rol: string;
+  contrasena?: string;
 }
+
+export const convertUserEditDTO = (user: EditUserSchema): UserEditDTO => {
+  return {
+    ...user,
+    contrasena: user.contrasena || undefined,
+  };
+};
