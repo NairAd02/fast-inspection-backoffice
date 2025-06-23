6ebbb14c9361e2de812d96227b9abd9ce674d5b0
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-23 12:20:48 -0400

    refactor(form): improve the structure of user forms, integrating reusable action buttons and adjusting the syntax for clarity

--- src/sections/user/form/edit/edit-user-form-container.tsx
+++ src/sections/user/form/edit/edit-user-form-container.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ModalContext } from "@/components/modal/context/modalContext";
 import { UserDetails } from "@/lib/types/user";
@@ -10,61 +10,55 @@ import { tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { EditUserSchema } from "./edit-user-schema";
-import { EdificationEdit } from "@/sections/edifications/form/edit/schemas/edification-edit-schema";
 import { modalTypes } from "@/components/modal/types/modalTypes";
 import { Form } from "@/components/ui/form";
 import UserForm from "../user-form";
-import { Button } from "@/components/ui/button";
-
+import FormActionButtons from "@/components/form/form-action-buttons/form-action-buttons";
 
 interface Props {
-    user: UserDetails;
+  user: UserDetails;
 }
 
-
 export default function EditUserFormContainer({ user }: Props) {
-    const { handleCloseModal } = use(ModalContext);
-    const { loading: submitLoading, action } = useEditUser({
-        id: user.id.toString(),
-        onEditAction: () => {
-            toast.success("Usuario editado con éxito");
-            handleClose();
-            revalidateServerTags(tagsCacheByRoutes.users.multipleTag);
-        },
-    });
-    const form = useForm<EditUserSchema>({
-        resolver: zodResolver(EditUserSchema),
-        defaultValues: {
-            email: user.email,
-            rol: user.rol as any,
-            contrasena: "",
-            nombreUsuario: user.nombreUsuario
-        },
-    });
+  const { handleCloseModal } = use(ModalContext);
+  const { loading: submitLoading, action } = useEditUser({
+    id: user.id.toString(),
+    onEditAction: () => {
+      toast.success("Usuario editado con éxito");
+      handleClose();
+      revalidateServerTags(tagsCacheByRoutes.users.multipleTag);
+    },
+  });
+  const form = useForm<EditUserSchema>({
+    resolver: zodResolver(EditUserSchema),
+    defaultValues: {
+      email: user.email,
+      rol: user.rol as any,
+      contrasena: "",
+      nombreUsuario: user.nombreUsuario,
+    },
+  });
 
-    const handleClose = () => {
-        handleCloseModal(modalTypes.editUserModal.name);
-    };
+  const handleClose = () => {
+    handleCloseModal(modalTypes.editUserModal.name);
+  };
 
-    function onSubmit(data: EditUserSchema) {
-        action(data);
-    }
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
-            >
-                <UserForm isEdit />
-                <div className="flex gap-2 justify-end">
-                    <Button type="button" variant={"outline"} onClick={handleClose}>
-                        Cancelar
-                    </Button>
-                    <Button variant={"default"} type="submit" disabled={submitLoading || !form.formState.isValid}>
-                        Editar Edificación
-                    </Button>
-                </div>
-            </form>
-        </Form>
-    );
+  function onSubmit(data: EditUserSchema) {
+    action(data);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="w-full flex flex-1 flex-col justify-between gap-8 h-full"
+      >
+        <UserForm isEdit />
+        <FormActionButtons
+          submitButtonText="Editar Usuario"
+          submitLoading={submitLoading}
+          handleClose={handleClose}
+        />
+      </form>
+    </Form>
+  );
 }
--- src/sections/user/form/new/new-user-form-container.tsx
+++ src/sections/user/form/new/new-user-form-container.tsx
@@ -1,59 +1,58 @@
-"use client"
+"use client";
 
-import { Form } from "@/components/ui/form"
-import { use, useCallback } from "react"
-import { useForm } from "react-hook-form"
-import UserForm from "../user-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { NewUserSchema } from "./new-user-schema"
-import useCreateUser from "../../hooks/use-create-user"
-import { toast } from "react-toastify"
-import { ModalContext } from "@/components/modal/context/modalContext"
-import { modalTypes } from "@/components/modal/types/modalTypes"
-import { revalidateServerTags } from "@/lib/cache"
-import { tagsCacheByRoutes } from "@/routes/api-routes/api-routes"
-import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form";
+import { use, useCallback } from "react";
+import { useForm } from "react-hook-form";
+import UserForm from "../user-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { NewUserSchema } from "./new-user-schema";
+import useCreateUser from "../../hooks/use-create-user";
+import { toast } from "react-toastify";
+import { ModalContext } from "@/components/modal/context/modalContext";
+import { modalTypes } from "@/components/modal/types/modalTypes";
+import { revalidateServerTags } from "@/lib/cache";
+import { tagsCacheByRoutes } from "@/routes/api-routes/api-routes";
+import FormActionButtons from "@/components/form/form-action-buttons/form-action-buttons";
 
 export default function NewUserFormContainer() {
-    const { handleCloseModal } = use(ModalContext);
-    const { loading, action } = useCreateUser({
-        onCreateAction: () => {
-            toast.success("Usuario creado con éxito");
-            handleClose();
-            revalidateServerTags(tagsCacheByRoutes.users.multipleTag);
-        },
-    })
+  const { handleCloseModal } = use(ModalContext);
+  const { loading, action } = useCreateUser({
+    onCreateAction: () => {
+      toast.success("Usuario creado con éxito");
+      handleClose();
+      revalidateServerTags(tagsCacheByRoutes.users.multipleTag);
+    },
+  });
 
-    const form = useForm<NewUserSchema>({
-        resolver: zodResolver(NewUserSchema),
-        defaultValues: {
-            nombreUsuario: "",
-            email: "",
-            contrasena: "",
-            rol: "Súper Administrador"
-        },
-        mode: "onChange"
-    })
+  const form = useForm<NewUserSchema>({
+    resolver: zodResolver(NewUserSchema),
+    defaultValues: {
+      nombreUsuario: "",
+      email: "",
+      contrasena: "",
+      rol: "Súper Administrador",
+    },
+    mode: "onChange",
+  });
 
-    const handleClose = () => {
-        handleCloseModal(modalTypes.newUserModal.name);
-    };
+  const handleClose = () => {
+    handleCloseModal(modalTypes.newUserModal.name);
+  };
 
-    const onSubmit = useCallback((data: NewUserSchema) => {
-        action(data)
-    }, [])
+  const onSubmit = useCallback((data: NewUserSchema) => {
+    action(data);
+  }, []);
 
-    return <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-            <UserForm />
-            <div className="flex gap-2 justify-end">
-                <Button type="button" variant="outline" onClick={handleClose}>
-                    Cancelar
-                </Button>
-                <Button variant={"default"} type="submit" disabled={loading || !form.formState.isValid}>
-                    Crear usuario
-                </Button>
-            </div>
-        </form>
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <UserForm />
+        <FormActionButtons
+          submitButtonText="Crear Usuario"
+          submitLoading={loading}
+          handleClose={handleClose}
+        />
+      </form>
     </Form>
+  );
 }
