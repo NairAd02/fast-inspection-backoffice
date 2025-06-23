9b84eb17e444bc5037e12920a467214312a3ef8d
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-23 12:57:17 -0400

    refactor(schema): improves password validation in the user editing scheme to ensure that it meets the minimum required length

--- src/sections/user/form/edit/edit-user-schema.ts
+++ src/sections/user/form/edit/edit-user-schema.ts
@@ -6,8 +6,9 @@ export const EditUserSchema = z.object({
   nombreUsuario: z.string().min(1, "El nombre de usuario es obligatorio"),
   contrasena: z
     .string()
-    .min(8, "La contraseña debe tener almenos 8 caracteres")
-    .optional(),
+    .refine((val) => val.length === 0 || val.length >= 8, {
+      message: "La contraseña debe tener al menos 8 caracteres",
+    }),
   email: z.string().email("El email debe ser válido"),
   rol: z.enum(Role),
 });
