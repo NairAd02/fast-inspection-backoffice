cd6af59862eec7f5f2b924f090791806f70f67e5
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 16:40:17 -0400

    feat(user-list): add user filter container and improve the structure of the user list component

--- src/sections/user/list/user-list.tsx
+++ src/sections/user/list/user-list.tsx
@@ -9,110 +9,117 @@ import { User } from "@/lib/types/user";
 import { ColumnDef } from "@tanstack/react-table";
 import { CheckCircle, EditIcon, EyeIcon, XCircle } from "lucide-react";
 import { use, useCallback } from "react";
+import UsersFiltersContainer from "../filters/users-filters-container";
 
 interface Props {
-    users: User[]
+  users: User[];
 }
 
 export default function UserList({ users }: Props) {
-    const { handleOpenModal } = use(ModalContext);
+  const { handleOpenModal } = use(ModalContext);
 
-    const handleEdit = useCallback(
-        (id: string) => {
-            handleOpenModal({
-                name: modalTypes.editUserModal.name,
-                entity: id,
-            });
-        },
-        [handleOpenModal]
-    );
+  const handleEdit = useCallback(
+    (id: string) => {
+      handleOpenModal({
+        name: modalTypes.editUserModal.name,
+        entity: id,
+      });
+    },
+    [handleOpenModal]
+  );
 
-    const handleViewDetails = useCallback(
-        (id: string) => {
-            handleOpenModal({
-                name: modalTypes.detailsUserModal.name,
-                entity: id,
-            });
-        },
-        [handleOpenModal]
-    );
+  const handleViewDetails = useCallback(
+    (id: string) => {
+      handleOpenModal({
+        name: modalTypes.detailsUserModal.name,
+        entity: id,
+      });
+    },
+    [handleOpenModal]
+  );
 
-    const columns: ColumnDef<User>[] = [
-        {
-            accessorKey: "id",
-            enableHiding: false,
-        },
-        {
-            accessorKey: "nombreUsuario",
-            header: "Nombre",
-        },
-        {
-            accessorKey: "email",
-            header: "Email",
-        },
-        {
-            accessorKey: "rol",
-            header: "Rol",
-        },
-        {
-            id: "estado",
-            cell: ({ row }) => {
-                return (
-                    <div className="flex items-center gap-3">
-                        {row.original.isActiva ? (
-                            <CheckCircle className="h-4 w-4 text-green-600" />
-                        ) : (
-                            <XCircle className="h-4 w-4 text-red-600" />
-                        )}
-                        <div className="flex-1">
-                            <p className="text-sm font-medium text-muted-foreground">Estado</p>
-                            <Badge variant={row.original.isActiva ? "default" : "destructive"} className="mt-1">
-                                {row.original.isActiva ? "Activa" : "Inactiva"}
-                            </Badge>
-                        </div>
-                    </div>
-                );
-            },
-        },
-        {
-            id: "actions",
-            cell: ({ row }) => {
-                return (
-                    <div className="flex justify-end">
-                        <TableMenu
-                            titleTableMenu="Acciones"
-                            actions={[
-                                {
-                                    label: "Ver Detalles",
-                                    icon: <EyeIcon />,
-                                    action: () => {
-                                        handleViewDetails(row.getValue("id"));
-                                    },
-                                },
-                                {
-                                    label: "Editar",
-                                    icon: <EditIcon />,
-                                    action: () => {
-                                        handleEdit(row.getValue("id"));
-                                    },
-                                }
-                            ]}
-                        />
-                    </div>
-                );
-            },
-        },
-    ];
-
-    return <div className="p-6 mb-6 bg-white flex flex-col gap-2 rounded-lg shadow-sm">
-        <div className="flex flex-col gap-4">
-            <DataTable
-                columns={columns}
-                data={users}
-                initialVisibilityState={{ id: false }}
-            // filters={<EdificationsFiltersContainer />}
+  const columns: ColumnDef<User>[] = [
+    {
+      accessorKey: "id",
+      enableHiding: false,
+    },
+    {
+      accessorKey: "nombreUsuario",
+      header: "Nombre",
+    },
+    {
+      accessorKey: "email",
+      header: "Email",
+    },
+    {
+      accessorKey: "rol",
+      header: "Rol",
+    },
+    {
+      id: "estado",
+      cell: ({ row }) => {
+        return (
+          <div className="flex items-center gap-3">
+            {row.original.isActiva ? (
+              <CheckCircle className="h-4 w-4 text-green-600" />
+            ) : (
+              <XCircle className="h-4 w-4 text-red-600" />
+            )}
+            <div className="flex-1">
+              <p className="text-sm font-medium text-muted-foreground">
+                Estado
+              </p>
+              <Badge
+                variant={row.original.isActiva ? "default" : "destructive"}
+                className="mt-1"
+              >
+                {row.original.isActiva ? "Activa" : "Inactiva"}
+              </Badge>
+            </div>
+          </div>
+        );
+      },
+    },
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        return (
+          <div className="flex justify-end">
+            <TableMenu
+              titleTableMenu="Acciones"
+              actions={[
+                {
+                  label: "Ver Detalles",
+                  icon: <EyeIcon />,
+                  action: () => {
+                    handleViewDetails(row.getValue("id"));
+                  },
+                },
+                {
+                  label: "Editar",
+                  icon: <EditIcon />,
+                  action: () => {
+                    handleEdit(row.getValue("id"));
+                  },
+                },
+              ]}
             />
+          </div>
+        );
+      },
+    },
+  ];
 
-        </div>
+  return (
+    <div className="p-6 mb-6 bg-white flex flex-col gap-2 rounded-lg shadow-sm">
+      <div className="flex flex-col gap-4">
+        <DataTable
+          columns={columns}
+          data={users}
+          initialVisibilityState={{ id: false }}
+          filters={<UsersFiltersContainer />}
+        />
+      </div>
     </div>
+  );
 }
