aed96781f123dedb7db82a5d73e8cbca36c93b46
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 17:49:40 -0400

    feat(sidebar): add SidebarHeaderContent component and adjust sidebar header to improve presentation

--- src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
+++ src/sections/dashboard-layout/components/app-sidebar/dashboard-layout-app-sidebar.tsx
@@ -7,13 +7,16 @@ import {
 } from "@/components/ui/sidebar";
 import { groupRoutes } from "@/routes/groups-routes/groups-routes";
 import SidebarGroupNavigation from "@/sections/dashboard-layout/components/app-sidebar/sidebar-group-navigation/sidebar-group-navigation";
+import SidebarHeaderContent from "../sidebar-header-content/sidebar-header-content";
 
 export function DashBoardLayoutAppSidebar({
   ...props
 }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar variant="floating" collapsible="icon" {...props}>
-      <SidebarHeader>Logo</SidebarHeader>
+      <SidebarHeader className="flex items-center justify-center">
+        <SidebarHeaderContent />
+      </SidebarHeader>
       <SidebarContent>
         {groupRoutes.map((groupRoute, index) => (
           <SidebarGroupNavigation key={index} group={groupRoute} />
