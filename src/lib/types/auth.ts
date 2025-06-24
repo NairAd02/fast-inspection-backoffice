71161a163c2cc8aea423f619e8bd5c20a459b411
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 12:31:25 -0400

    feat(auth): add function to convert VerifyCodeDTO from VerifyCode and User ID

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,3 +1,5 @@
+import { VerifyCode } from "@/sections/auth/form/verify-code/shcemas/verify-code-schema";
+
 export interface CredentialsDTO {
   nombreUsuario: string;
   contrasena: string;
@@ -7,3 +9,13 @@ export interface VerifyCodeDTO {
   idUsuario: string;
   codigoActivacion: string;
 }
+
+export const convertVerifyCodeDTO = (
+  verifyCode: VerifyCode,
+  userId: string
+): VerifyCodeDTO => {
+  return {
+    ...verifyCode,
+    idUsuario: userId,
+  };
+};
