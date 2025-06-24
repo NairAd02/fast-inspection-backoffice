94e253d8f2e51725a651f26e81321c3e9b352e2a
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 13:28:55 -0400

    feat(auth): add interface and function to handle forgotten password change

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,3 +1,4 @@
+import { ChangePasswordForgot } from "@/sections/auth/form/change-password-forgot/schemas/change-password-forgot-schema";
 import { VerifyCode } from "@/sections/auth/form/verify-code/shcemas/verify-code-schema";
 
 export interface CredentialsDTO {
@@ -10,6 +11,10 @@ export interface VerifyCodeDTO {
   codigoActivacion: string;
 }
 
+export interface ChangepasswordForgotDTO {
+  contrasena: string;
+}
+
 export const convertVerifyCodeDTO = (
   verifyCode: VerifyCode,
   userId: string
@@ -17,5 +22,13 @@ export const convertVerifyCodeDTO = (
   return {
     ...verifyCode,
     idUsuario: userId,
+  };
+};
+
+export const convertChangePasswordForgotDTO = (
+  changePasswordForgot: ChangePasswordForgot
+): ChangepasswordForgotDTO => {
+  return {
+    contrasena: changePasswordForgot.password,
   };
 };
