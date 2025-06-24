b5f6b4927850375b869522651729f31d8e6db13f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 12:06:55 -0400

    feat(auth): add the function to verify the user's identity code

--- src/lib/services/auth.ts
+++ src/lib/services/auth.ts
@@ -4,7 +4,7 @@ import { signIn as signInAuth } from "@/auth";
 import { buildApiResponse } from "@/lib/api";
 import { apiRoutes } from "@/routes/api-routes/api-routes";
 import { Session } from "next-auth";
-import { CredentialsDTO } from "../types/auth";
+import { CredentialsDTO, VerifyCodeDTO } from "../types/auth";
 
 export async function login(credentials: CredentialsDTO) {
   const res = await fetch(apiRoutes.auth.login, {
@@ -36,6 +36,18 @@ export async function sendVerificationCode(userId: string) {
       },
     }
   );
+
+  return await buildApiResponse<{ success: boolean }>(res);
+}
+
+export async function verifyCode(verifyCodeDTO: VerifyCodeDTO) {
+  const res = await fetch(apiRoutes.auth.verifyCode, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(verifyCodeDTO),
+  });
 
   return await buildApiResponse<{ success: boolean }>(res);
 }
