8d5eb904b878469ff7c24a2136a1ec9b1db0d868
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 22:15:46 -0400

    feat(feature-card): update the style of the FeatureCard component, changing the background color to 'bg-primary' and the icon color to white

--- src/components/feature-card/feature-card.tsx
+++ src/components/feature-card/feature-card.tsx
@@ -12,11 +12,11 @@ export default function FeatureCard({ title, desciption, icon }: Props) {
   return (
     <Card className="border-0 shadow-lg hover:shadow-xl transition-shadow">
       <CardHeader>
-        <div className="w-12 h-12 bg-blue-100 rounded-lg flex items-center justify-center mb-4">
+        <div className="w-12 h-12 bg-primary rounded-lg flex items-center justify-center mb-4">
           {icon ? (
             icon
           ) : (
-            <ClipboardCheckIcon className="h-6 w-6 text-blue-600" />
+            <ClipboardCheckIcon className="h-6 w-6 text-white" />
           )}
         </div>
         <CardTitle>{title}</CardTitle>
