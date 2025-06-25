1c6f6315dfda2121bd8cd779e1da05f520999d31
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 23:19:13 -0400

    feat(edifications-container): replace the FileCog icon with Building in the EdificationsContainer component to improve visual representation.

--- src/sections/edifications/edifications-container.tsx
+++ src/sections/edifications/edifications-container.tsx
@@ -1,5 +1,5 @@
 import SectionsHeader from "@/components/sections-header/sections-header";
-import { FileCogIcon } from "lucide-react";
+import { Building } from "lucide-react";
 import React from "react";
 import EdificationsListContainer from "./list/edifications-list-container";
 import { SearchParamsPagination } from "@/lib/types/pagination";
@@ -13,14 +13,14 @@ export default function EdificationsContainer({ searchParams }: Props) {
   return (
     <div className="flex flex-col gap-4">
       <SectionsHeader
-        sectionIcon={<FileCogIcon />}
+        sectionIcon={<Building />}
         sectionTitle="Gestión de Edificaciones"
         sectionDescription="Gestione la edificaciones usadas para la inspección de deterioros"
-         addButton={{
-           isModalRedirect: true,
-           buttonText: "Nueva Edificación",
-           creationPath: modalTypes.newEdificationModal.name,
-         }}
+        addButton={{
+          isModalRedirect: true,
+          buttonText: "Nueva Edificación",
+          creationPath: modalTypes.newEdificationModal.name,
+        }}
       />
       <EdificationsListContainer searchParams={searchParams} />
     </div>
