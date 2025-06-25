30eb6a21240c63d488ca2f3d1e0ab3213b8982a3
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-25 02:51:25 -0400

    feat(edification-details-inspection-history): add refresh button and improve inspection history structure

--- src/sections/edifications/details/edification-details-inspection-history/edification-details-inspection-history.tsx
+++ src/sections/edifications/details/edification-details-inspection-history/edification-details-inspection-history.tsx
@@ -1,9 +1,11 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import TooltipContainer from "@/components/ui/tooltip-container";
 import InspectionCard from "@/sections/inspections/components/inspection-card/inspection-card";
 import InspectionCardSkeleton from "@/sections/inspections/components/inspection-card/inspection-card-skeleton/inspection-card-skeleton";
 import InspectionsFiltersContainer from "@/sections/inspections/filters/inspections-filters-container";
 import useInspections from "@/sections/inspections/hooks/use-inspections";
-import { ActivityIcon, BarChart3 } from "lucide-react";
+import { ActivityIcon, BarChart3, RefreshCcw } from "lucide-react";
 import React from "react";
 
 interface Props {
@@ -13,29 +15,44 @@ interface Props {
 export default function EdificationDetailsInspectionHistory({
   edificationId,
 }: Props) {
-  const { inspections, loadingData, filters } = useInspections({
-    defaultsFilters: {
-      edificacionId: edificationId,
-    },
-  });
+  const { inspections, loadingData, filters, fetchInspections } =
+    useInspections({
+      defaultsFilters: {
+        edificacionId: edificationId,
+      },
+    });
 
   return (
     <div className="mb-6 flex flex-col gap-4">
       <div className="flex flex-col gap-2">
-      <div className="flex items-center gap-2 mb-4">
-        <div className="w-6 h-6 bg-indigo-100 rounded-lg flex items-center justify-center">
-          <BarChart3 className="w-4 h-4 text-indigo-600" />
+        <div className="flex items-center gap-2 mb-4">
+          <div className="w-6 h-6 bg-indigo-100 rounded-lg flex items-center justify-center">
+            <BarChart3 className="w-4 h-4 text-indigo-600" />
+          </div>
+          <h2 className="text-lg font-semibold text-gray-900">
+            Historial de Inspecciones
+          </h2>
         </div>
-        <h2 className="text-lg font-semibold text-gray-900">
-          Historial de Inspecciones
-        </h2>
-      </div>
-      <InspectionsFiltersContainer
-        filters={filters.filters}
-        handleChangeFilters={filters.handleChangeFilters}
-        handleResetFilters={filters.handleResetFilters}
-        getActiveFiltersCount={filters.getActiveFiltersCount}
-      />
+        <div className="flex justify-between gap-2 items-center">
+          <InspectionsFiltersContainer
+            filters={filters.filters}
+            handleChangeFilters={filters.handleChangeFilters}
+            handleResetFilters={filters.handleResetFilters}
+            getActiveFiltersCount={filters.getActiveFiltersCount}
+          />
+          <TooltipContainer content="Refrescar Inspecciones">
+            <Button
+              variant="outline"
+              size="icon"
+              className="rounded-full"
+              onClick={() => {
+                fetchInspections();
+              }}
+            >
+              <RefreshCcw className="w-4 h-4" />
+            </Button>
+          </TooltipContainer>
+        </div>
       </div>
       {loadingData ? (
         <div className="space-y-4">
