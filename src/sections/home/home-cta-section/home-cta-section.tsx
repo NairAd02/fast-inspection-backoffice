dfbe4ce896db0db364afaa4f58f68909dfd4fc16
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-24 22:22:11 -0400

    feat(home-cta-section): integrate the NavigationComponent and update the text styles to white to improve visibility in the call-to-action section.

--- src/sections/home/home-cta-section/home-cta-section.tsx
+++ src/sections/home/home-cta-section/home-cta-section.tsx
@@ -1,28 +1,25 @@
+import NavigationComponent from "@/components/navigation-component/navigation-component";
 import { Button } from "@/components/ui/button";
+import { paths } from "@/routes/path";
 import React from "react";
 
 export default function HomeCtaSection() {
   return (
-    <section className="py-20 px-4 bg-blue-600 text-white">
+    <section className="py-20 px-4 bg-primary ">
       <div className="container mx-auto text-center">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">
+        <h2 className="text-3xl md:text-4xl font-bold mb-4 text-white">
           ¿Listo para transformar tus inspecciones?
         </h2>
-        <p className="text-xl mb-8 opacity-90 max-w-2xl mx-auto">
+        <p className="text-xl mb-8 opacity-90 max-w-2xl mx-auto text-white">
           Únete a cientos de profesionales que ya confían en nuestra plataforma
           para gestionar sus inspecciones de manera más eficiente.
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <Button size="lg" variant="secondary" className="text-lg px-8 py-3">
-            Comenzar Prueba Gratuita de 14 Días
-          </Button>
-          <Button
-            size="lg"
-            variant="outline"
-            className="text-lg px-8 py-3 border-white text-white hover:bg-white hover:text-blue-600"
-          >
-            Hablar con Ventas
-          </Button>
+          <NavigationComponent href={paths.configs.root}>
+            <Button size="lg" variant="secondary" className="text-lg px-8 py-3">
+              Comenzar
+            </Button>
+          </NavigationComponent>
         </div>
       </div>
     </section>
