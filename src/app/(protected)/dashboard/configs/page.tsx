9957bb8253eb1638810ff9ae5717eb384de588a4
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-27 22:00:03 -0400

    feat(configs-page): add metadata for the title and description of the settings management page

--- src/app/(protected)/dashboard/configs/page.tsx
+++ src/app/(protected)/dashboard/configs/page.tsx
@@ -1,9 +1,15 @@
 import { SearchParamsPagination } from "@/lib/types/pagination";
 import ConfigsContainer from "@/sections/configs/configs-container";
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {
   searchParams: Promise<SearchParamsPagination>;
+};
+
+export const metadata: Metadata = {
+  title: "Gestión de Configuraciones",
+  description: "Administre las configuraciones que serán usadas en las inspecciones",
 };
 
 export default async function ConfigsPage({ searchParams }: Props) {
