4e65b835ff94e351f630967e50da1d739c10493e
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-27 22:05:57 -0400

    feat(inspections-page): add metadata for the title and description of the inspection details page

--- src/app/(protected)/dashboard/inspections/[id]/page.tsx
+++ src/app/(protected)/dashboard/inspections/[id]/page.tsx
@@ -1,9 +1,16 @@
 import { getInspectionById } from "@/lib/services/inspections";
 import InspectionDetailsContainer from "@/sections/inspections/details/inspection-details-container";
+import { Metadata } from "next";
 import React from "react";
 
 type Props = {
   params: Promise<{ id: string }>;
+};
+
+export const metadata: Metadata = {
+  title: "Detalles de Inspección",
+  description:
+    "Visualice los detalles y realice análisis sobre la inspección especificada",
 };
 
 export default async function InspectionPage({ params }: Props) {
