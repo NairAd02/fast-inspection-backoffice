60a685391f79cae7efb039eedce3533c6061765f
NairAd02 <adrian.suarez.alphen@gmail.com>
2025-06-28 21:36:29 -0400

    feat(users): add icon to modals of users page

--- src/app/(protected)/dashboard/user/page.tsx
+++ src/app/(protected)/dashboard/user/page.tsx
@@ -5,39 +5,44 @@ import UserDetailsModalContainer from "@/sections/user/details/user-details-moda
 import EditUserModalContainer from "@/sections/user/form/edit/edit-user-form-modal-container";
 import NewUserFormContainer from "@/sections/user/form/new/new-user-form-container";
 import UserContainer from "@/sections/user/user-container";
+import { User } from "lucide-react";
 
 interface Props {
-    searchParams: Promise<SearchParamsPagination>
+  searchParams: Promise<SearchParamsPagination>;
 }
 
 export default async function UserPage({ searchParams }: Props) {
+  return (
+    <>
+      <UserContainer searchParams={await searchParams} />
+      <Modal
+        formPath={modalTypes.newUserModal.name}
+        title={modalTypes.newUserModal.title}
+        maxWidth="max-w-2xl"
+        icon={<User />}
+      >
+        <NewUserFormContainer />
+      </Modal>
 
-    return <>
-        <UserContainer searchParams={await searchParams} />
-        <Modal
-            formPath={modalTypes.newUserModal.name}
-            title={modalTypes.newUserModal.title}
-            maxWidth="max-w-2xl"
-        >
-            <NewUserFormContainer />
-        </Modal>
+      <Modal
+        formPath={modalTypes.editUserModal.name}
+        title={modalTypes.editUserModal.title}
+        maxWidth="max-w-2xl"
+        className="min-h-[36vh]"
+        icon={<User />}
+      >
+        <EditUserModalContainer />
+      </Modal>
 
-        <Modal
-            formPath={modalTypes.editUserModal.name}
-            title={modalTypes.editUserModal.title}
-            maxWidth="max-w-2xl"
-            className="min-h-[36vh]"
-        >
-            <EditUserModalContainer />
-        </Modal>
-
-        <Modal
-            formPath={modalTypes.detailsUserModal.name}
-            title={modalTypes.detailsUserModal.title}
-            maxWidth="max-w-2xl"
-            className="min-h-[37vh] max-h-[90vh]"
-        >
-            <UserDetailsModalContainer />
-        </Modal>
+      <Modal
+        formPath={modalTypes.detailsUserModal.name}
+        title={modalTypes.detailsUserModal.title}
+        maxWidth="max-w-2xl"
+        className="min-h-[37vh] max-h-[90vh]"
+        icon={<User />}
+      >
+        <UserDetailsModalContainer />
+      </Modal>
     </>
+  );
 }
